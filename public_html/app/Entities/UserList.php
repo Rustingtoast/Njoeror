f181d4b6fdf92c253d9dc040010394f622fd6630
Inuyashatoast <nalex.ne@gmail.com>
2026-01-29 20:49:59 +0000

    added get operators

--- public_html/app/Entities/UserList.php
+++ public_html/app/Entities/UserList.php
@@ -7,6 +7,28 @@ use CodeIgniter\Entity\Entity;
 class UserList extends Entity
 {
     private $id;
-    private $full_Name;
+    private $full_name;
     private $email;
+
+    public function __construct($input_id, $input_name, $input_email)
+    {
+        $this->id = $input_id;
+        $this->full_name = $input_name;
+        $this->email = $input_email;
+    }
+
+    public function getID()
+    {
+        return $this->id;
+    }
+
+    public function getFullName()
+    {
+        return $this->full_name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
 }
